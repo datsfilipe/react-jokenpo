39dae4e07023f81837163c02ad77402702b66a31
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-31 14:52:14 -0300

    feat(types): declaring more global types to the game

--- src/types/game.d.ts
+++ src/types/game.d.ts
@@ -1,1 +1,24 @@
 declare type GuessTypes = 'scissors' | 'rocks' | 'papers'
+
+declare type Option = {
+  value: string;
+  x: number;
+  y: number;
+  id: string;
+}
+
+declare type OptionsState = {
+  scissors: Option[];
+  rocks: Option[];
+  papers: Option[];
+  [key: string]: Option[];
+}
+
+declare type Game = {
+  guess: GuessTypes;
+  setGuess: React.Dispatch<React.SetStateAction<GuessTypes>>;
+  options: OptionsState;
+  dispatch: React.Dispatch<Action>;
+  playing: boolean;
+  setPlaying: React.Dispatch<React.SetStateAction<boolean>>;
+}
