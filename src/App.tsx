0a15f8f56e45d5f4bbb218a69f99aad62a1b8654
datsfilipe <datsfilipe.dev@gmail.com>
2022-10-31 14:57:51 -0300

    feat: replace the logic with the context states in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,263 +1,159 @@
-import { useState, useEffect, memo } from "react";
-import "./App.css";
-import { BottomContent } from "./components/BottomContent";
+import { GameContext } from './contexts/GameContext'
+import { ActionType, handleHit } from './contexts/GameContext'
+import { memo, useEffect, useContext } from 'react'
+import { BottomContent } from './components/BottomContent'
 
-type Option = {
-  value: string;
-  x: number;
-  y: number;
-  id: string;
-};
+import './App.css'
 
 function App() {
-  const [guess, setGuess] = useState<GuessTypes>("scissors");
-  const [playing, setPlaying] = useState(false);
-  const [scissors, setScissors] = useState<Option[]>([]);
-  const [rocks, setRocks] = useState<Option[]>([]);
-  const [papers, setPapers] = useState<Option[]>([]);
+  const box = document.getElementById('game-box')
+  const position = box?.getBoundingClientRect()
+  const left = position?.left
+  const top = position?.top
+  const right = position?.right
+  const bottom = position?.bottom
+
+  const {
+    guess,
+    setGuess,
+    playing,
+    setPlaying,
+    options,
+    dispatch
+  } = useContext(GameContext)
 
   const checkVictory = () => {
-    const options = { scissors, rocks, papers };
-    const selected = options[guess];
+    const { scissors, rocks, papers } = options
+    const selected = options[guess]
     const enemy = guess === 'scissors'
-      ? options.rocks : guess === 'rocks'
-      ? options.papers : options.scissors;
-    const other = Object.values(options).filter(option => option !== selected && option !== enemy);
+      ? rocks : guess === 'rocks'
+        ? papers : scissors
+    const other = Object.values(options).filter(option => option !== selected && option !== enemy)
     
     if (selected.length === 0 || enemy.length > 0 && other.length === 0) {
-      alert("You lose!");
-      setPlaying(false);
+      alert('You lose!')
+      setPlaying(false)
     } else if (enemy.length === 0) {
-      alert("You win!");
-      setPlaying(false);
+      alert('You win!')
+      setPlaying(false)
     }
-  };
+  }
 
-  const box = document.getElementById("game-box");
-  const position = box?.getBoundingClientRect();
-  const left = position?.left;
-  const top = position?.top;
-  const right = position?.right;
-  const bottom = position?.bottom;
+  useEffect(() => {
+    if (playing) {
+      checkVictory()
+    }
+
+    const interval = setInterval(() => {
+      if (playing && left && top && right && bottom) {
+        dispatch({
+          type: ActionType.SET_OPTIONS,
+          payload: options,
+          left,
+          top,
+          right,
+          bottom
+        })
+      }
+    }, 50)
+
+    return () => clearInterval(interval)
+  }, [playing, options])
 
   useEffect(() => {
     if (left && top && right && bottom) {
-      setScissors(() => {
-        let value = [];
-        for (let i = 0; i < 10; i++) {
-          value.push({
-            value: "✂️",
-            x: Math.floor(Math.random() * (right - left) + left),
-            y: Math.floor(Math.random() * (bottom - top) + top),
-            id: i + "scissor",
-          });
-        }
-        return value;
-      });
-
-      setRocks(() => {
-        let value = [];
-        for (let i = 0; i < 10; i++) {
-          value.push({
-            value: "🪨",
-            x: Math.floor(Math.random() * (right - left) + left),
-            y: Math.floor(Math.random() * (bottom - top) + top),
-            id: i + "rock",
-          });
-        }
-        return value;
-      });
-
-      setPapers(() => {
-        let value = [];
-        for (let i = 0; i < 10; i++) {
-          value.push({
-            value: "📄",
-            x: Math.floor(Math.random() * (right - left) + left),
-            y: Math.floor(Math.random() * (bottom - top) + top),
-            id: i + "paper",
-          });
-        }
-        return value;
-      });
+      dispatch({
+        type: ActionType.CREATE_OPTIONS,
+        left,
+        top,
+        right,
+        bottom
+      })
     }
-  }, [guess, left, top, right, bottom]);
-
-  const handleHit = (option: Option, optionList: Option[]) => {
-    // return true or false for hit
-    const hit = optionList.some(item => {
-      return (
-        option.x > item.x - 20 &&
-        option.x < item.x + 20 &&
-        option.y > item.y - 20 &&
-        option.y < item.y + 20
-      );
-    });
-
-    return hit;
-  };
-
-  const handleTransferItem = (
-    option: Option,
-    optionList: Option[],
-    optionList2: Option[],
-    value: string
-  ) => {
-    const index = optionList.findIndex(item => item.id === option.id);
-    const newList = [...optionList];
-    newList.splice(index, 1);
-    const newItem = {
-      ...option,
-      value,
-    };
-    optionList = newList;
-    optionList2.push(newItem);
-    return [optionList, optionList2];
-  };
+  }, [playing, guess])
 
   useEffect(() => {
-    if (!playing) return;
-    const timeout = setTimeout(() => {
-      if (left && top && right && bottom) {
-        let newScissors = [...scissors];
-        let newRocks = [...rocks];
-        let newPapers = [...papers];
-
-        newScissors.forEach(() => {
-          let value = newScissors.map(item => {
-            const x = Math.round(Math.random() * 10 - 5 + item.x);
-            const y = Math.round(Math.random() * 10 - 5 + item.y);
-            return {
-              ...item,
-              x: x > left && x < right ? x : item.x,
-              y: y > top && y < bottom ? y : item.y,
-            };
-          });
-
-          value.forEach(item => {
-            if (handleHit(item, rocks)) {
-              const [newScissorsValue, newRocksValue] = handleTransferItem(
-                item,
-                value,
-                newRocks,
-                "🪨"
-              );
-              value = newScissorsValue;
-              newRocks = newRocksValue;
-            }
-          });
-
-          newScissors = value;
-        });
-
-        newRocks.forEach(() => {
-          let value = newRocks.map(item => {
-            const x = Math.round(Math.random() * 10 - 5 + item.x);
-            const y = Math.round(Math.random() * 10 - 5 + item.y);
-            return {
-              ...item,
-              x: x > left && x < right ? x : item.x,
-              y: y > top && y < bottom ? y : item.y,
-            };
-          });
-
-          value.forEach(item => {
-            if (handleHit(item, papers)) {
-              const [newRocksValue, newPapersValue] = handleTransferItem(
-                item,
-                value,
-                newPapers,
-                "📄"
-              );
-              value = newRocksValue;
-              newPapers = newPapersValue;
-            }
-          });
-
-          newRocks = value;
-        });
-
-        newPapers.forEach(() => {
-          let value = newPapers.map(item => {
-            const x = Math.round(Math.random() * 10 - 5 + item.x);
-            const y = Math.round(Math.random() * 10 - 5 + item.y);
-            return {
-              ...item,
-              x: x > left && x < right ? x : item.x,
-              y: y > top && y < bottom ? y : item.y,
-            };
-          });
-
-          value.forEach(item => {
-            if (handleHit(item, scissors)) {
-              const [newPapersValue, newScissorsValue] = handleTransferItem(
-                item,
-                value,
-                newScissors,
-                "✂️"
-              );
-              value = newPapersValue;
-              newScissors = newScissorsValue;
-            }
-          });
-
-          newPapers = value;
-        });
-
-        setScissors(newScissors);
-        setRocks(newRocks);
-        setPapers(newPapers);
+    if (playing && left && top && right && bottom) {
+      for(let i = 0; i < options.papers.length; i++) {
+        const paper = options.papers[i]
+        const hit = handleHit(paper, options.scissors)
+        console.log(hit)
+        if (hit) {
+          dispatch({
+            type: ActionType.TRANSFER_OPTIONS,
+            payload: paper,
+            to: 'scissors'
+          })
+        }
       }
 
-      checkVictory();
-    }, 250);
+      for(let i = 0; i < options.rocks.length; i++) {
+        const rock = options.rocks[i]
+        const hit = handleHit(rock, options.papers)
+        if (hit) {
+          dispatch({
+            type: ActionType.TRANSFER_OPTIONS,
+            payload: rock,
+            to: 'papers'
+          })
+        }
+      }
 
-    return () => {
-      clearTimeout(timeout);
-    };
-  }, [playing, scissors, rocks, papers]);
+      for(let i = 0; i < options.scissors.length; i++) {
+        const scissors = options.scissors[i]
+        const hit = handleHit(scissors, options.rocks)
+        if (hit) {
+          dispatch({
+            type: ActionType.TRANSFER_OPTIONS,
+            payload: scissors,
+            to: 'rocks'
+          })
+        }
+      }
+    }
+  }, [options, playing])
 
   return (
     <div className='App'>
       <div className='container'>
         <div className='game-box' id='game-box'>
-          {scissors.map(scissor => (
+          {options.scissors.map(scissor => (
             <div
               key={scissor.id}
               className='option'
               style={{
-                position: "absolute",
+                position: 'absolute',
                 left: scissor.x,
                 top: scissor.y,
-                transition: "0.25s all linear",
+                transition: '0.25s all linear',
               }}
             >
               {scissor.value}
             </div>
           ))}
-          {rocks.map(rock => (
+          {options.rocks.map(rock => (
             <div
               key={rock.id}
               className='option'
               style={{
-                position: "absolute",
+                position: 'absolute',
                 left: rock.x,
                 top: rock.y,
-                transition: "0.25s all linear",
+                transition: '0.25s all linear',
               }}
             >
               {rock.value}
             </div>
           ))}
-          {papers.map(paper => (
+          {options.papers.map(paper => (
             <div
               key={paper.id}
               className='option'
               style={{
-                position: "absolute",
+                position: 'absolute',
                 left: paper.x,
                 top: paper.y,
-                transition: "0.25s all linear",
+                transition: '0.25s all linear',
               }}
             >
               {paper.value}
@@ -271,7 +167,7 @@ function App() {
         setPlaying={setPlaying}
       />
     </div>
-  );
+  )
 }
 
-export default memo(App);
+export default memo(App)
