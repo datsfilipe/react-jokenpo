b0badab0072f8f451cbdeebdba5c836f7b1620ef
datsfilipe <datsfilipe.foss@proton.me>
2024-01-06 19:42:48 -0300

    refactor: moving files, update project folders

--- src/components/BottomContent/PlayButton.tsx
+++
@@ -1,9 +0,0 @@
-import { useContext } from 'react'
-import { GameContext } from '../../contexts/GameContext'
-import style from './BottomContent.module.css'
-
-export const PlayButton = () => {
-  const { setPlaying, playing } = useContext(GameContext)
-
-  return <button className={style.controlBtn} onClick={() => setPlaying(true)} disabled={playing}>Play</button>
-}
